e0f725db9d0297de56cfbe34cf3e3b34a488f6b6
Ingrid04 <ingridfmelgar@gmail.com>
2021-03-27 00:06:02 -0400

    backend and frontend connected

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// require('dotenv').config();
+require('dotenv').config();
 const bodyParser = require('body-parser');
 const path = require('path');
 const gameRoutes = require('./routes/routes');
@@ -9,7 +9,7 @@ const pool = require('./db');
 
 app.use(bodyParser.json());
 
-app.use('/games',gameRoutes);
+app.use('/api/games',gameRoutes);
 
 
 app.get("/", (req, res) => {
@@ -17,21 +17,9 @@ app.get("/", (req, res) => {
     res.json('NBA GAMES SCORES');
   });
 
-//   const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}.`);
-// });
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  const url = `http://localhost:${PORT}`
+  console.log(`Server is running ${url}.`);
+});
 
-app.listen(5000);
-
-// const mysql = require('mysql');
-// const connection = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'mysql09',
-//   database: 'react-express-sql-app'
-// });
-// connection.connect((err) => {
-//   if (err) throw err;
-//   console.log('Connected!');
-// });
